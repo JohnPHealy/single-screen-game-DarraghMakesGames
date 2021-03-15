ed2fcf6d1e980d92b6f05f876bb7b8d049766091
DarraghMakesGames <DarraghMakesGames@users.noreply.github.com>
2021-03-15 14:30:10 +0000

    Added basic player movement scripts (run & jump)

--- Sooie/Assets/Scripts/PlayerMovement.cs
+++ Sooie/Assets/Scripts/PlayerMovement.cs
@@ -5,15 +5,51 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float moveForce, maxSpeed, jumpForce;
+    [SerializeField] private Collider2D groundCheck;
+    [SerializeField] private LayerMask groundLayers;
+    private float moveDirection;
+    private Rigidbody2D playerRB;
+    private bool canJump;
+
+    private void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Mathf.Abs(playerRB.velocity.x) < maxSpeed)
+        {
+            var moveAxis = Vector2.right * moveDirection;
+            playerRB.AddForce(moveAxis * moveForce, ForceMode2D.Force);
+        }
+
+        if (groundCheck.IsTouchingLayers(groundLayers))
+        {
+            canJump = true;
+        }
+        else
+        {
+            canJump = false;
+        }
+
+    }
 
     public void Move(InputAction.CallbackContext context)
     {
-        print(context.ReadValue<float>());
+        //print(context.ReadValue<float>());
+        moveDirection = context.ReadValue<float>();
     }
 
     public void Jump(InputAction.CallbackContext context)
     {
-        print(context.ReadValue<float>());
+        //print(context.ReadValue<float>());
+        if (canJump)
+        {
+            playerRB.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            //canJump = false;
+        }
     }
 
 }
